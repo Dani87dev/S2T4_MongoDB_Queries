c6362740399497931d167f81b826a39fb24528de
Dani Vila <danivila.dev@gmail.com>
2026-02-11 10:53:42 +0100

    querie 2 mongoDB

--- query/queries.js
+++ query/queries.js
@@ -2,7 +2,7 @@
 db.restaurants.find();
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find({}, {_id:0, restaurant_id:1, name:1});
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
 
