094d91b4b8770d0df13fa21af61e18b19915c92d
Dani Vila <danivila.dev@gmail.com>
2026-02-17 09:08:27 +0100

    Refactor MongoDB queries for better readability

--- query/queries.js
+++ query/queries.js
@@ -25,16 +25,17 @@ db.restaurants.find({"borough" : "Bronx"}, {_id: 0}).skip(5).limit(5);
 db.restaurants.find({"grades.score" : {"$gt" : 90}}, {"_id":0});
 
 // 9. Trobar els restaurants amb un score de més de 80 però menys que 100.
-db.restaurants.find({"grades.score": {$gt: 80, $lt: 100}}).sort({restaurant_id: 1 });
+db.restaurants.find({grades:{$elemMatch:{score:{$gt:80,$lt:100}}}},{_id:0}).sort({restaurant_id:1});
+
 
 // 10. Trobar els restaurants amb longitud menor que -95.754168.
-db.restaurants.find({"address.coord.0": {$lt:-95.754168}});
+db.restaurants.find({"address.coord.0":pots 
 
 // 11. Trobar restaurants que no preparen 'American', amb qualificació > 70 i longitud < -65.754168.
-db.restaurants.find({$and:[{cuisine: {$ne:"American"}}, {"grades.score": {$gt:70}}, {"address.coord.0": {$lt:-65.754168}}]});
+db.restaurants.find({cuisine:{$ne:"American"},grades:{$elemMatch:{score:{$gt:70}}},"address.coord.0":{$lt:-65.754168}},{_id:0});
 
 // 12. El mateix que l'anterior però sense usar operador $and.
-db.restaurants.find({cuisine: {$ne:"American"}, "grades.score": {$gt:70}, "address.coord.0": {$lt:-65.754168}});
+db.restaurants.find({cuisine:{$ne:"American"},grades:{$elemMatch:{score:{$gt:70}}},"address.coord.0":{$lt:-65.754168}},{_id:0});
 
 // 13. Trobar restaurants que no són 'American', grau 'A', i no són de Brooklyn. Ordenats per cuisine descendent.
 db.restaurants.find({ cuisine: { $ne: "American" }, "grades.grade": "A", borough: { $ne: "Brooklyn" } }, { _id: 0 }).sort({ cuisine: -1 });
